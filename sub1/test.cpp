a15ad42bf86cd599b3c30673b5b5b46272d7571d
xiaojuren <2890581080@qq.com>
2025-07-05 17:18:51 +0800

    sub2

--- sub1/test.cpp
+++ sub1/test.cpp
@@ -2,10 +2,17 @@
 using namespace std;
 int main()
 {
+    vector<char> vt;
     string s;
     cin >> s;
     for (auto& x : s) {
         cout << x << ' ';
+        vt.push_back(x);
     }
+    cout << '\n';
+    for (char x : vt) {
+        cout << x << ' ';
+    }
+    cout << '\n';
     return 0;
 }

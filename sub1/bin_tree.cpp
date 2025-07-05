b10357eaf923236d9f98f8535b31eeb1bc6e8340
xiaojuren <2890581080@qq.com>
2025-07-05 17:26:39 +0800

    sub3

---
+++ sub1/bin_tree.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef struct BinTree {
+    char value;
+    struct BinTree* lchild;
+    struct BinTree* rchild;
+} *BT, bt;
+int main()
+{
+    BT head = NULL;
+    return 0;
+}
